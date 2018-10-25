9573c6c3df593438f66962339d21fd0c729d9d75
Marcelo Schneider <schneider.fei@gmail.com>
2018-10-25 18:25:30 -0300

    Adding javascript wrapper to fields - issue #1

--- src/Element/WebformViacepAddressComposite.php
+++ src/Element/WebformViacepAddressComposite.php
@@ -3,6 +3,7 @@
 namespace Drupal\viacep_address_composite\Element;
 
 use Drupal\webform\Element\WebformCompositeBase;
+use Drupal\Component\Utility\Html;
 
 /**
  * Provides a webform element for an address element.
@@ -31,36 +32,45 @@ class WebformViacepAddressComposite extends WebformCompositeBase {
    * {@inheritdoc}
    */
   public static function getCompositeElements(array $elements) {
+    
+    $html_id = Html::getUniqueId('webform_example_composite');
+
     $elements = [];
 
     $elements['postal_code'] = [
       '#type' => 'textfield',
       '#title' => t('CEP'),
+      '#attributes' => ['data-webform-composite-id' => $html_id . '--postal_code'],
     ];
 
     $elements['address'] = [
       '#type' => 'textfield',
-      '#title' => t('Endereço')
+      '#title' => t('Endereço'),
+      '#attributes' => ['data-webform-composite-id' => $html_id . '--address'],
     ];
 
     $elements['address_number'] = [
       '#type' => 'textfield',
       '#title' => t('Número'),
+      '#attributes' => ['data-webform-composite-id' => $html_id . '--address_number'],
     ];
 
     $elements['neighborhood'] = [
       '#type' => 'textfield',
-      '#title' => t('Bairro')
+      '#title' => t('Bairro'),
+      '#attributes' => ['data-webform-composite-id' => $html_id . '--neighborhood'],
     ];
 
     $elements['city'] = [
       '#type' => 'textfield',
       '#title' => t('Cidade'),
+      '#attributes' => ['data-webform-composite-id' => $html_id . '--city'],
     ];
 
     $elements['state_province'] = [
       '#type' => 'textfield',
       '#title' => t('Estado'),
+      '#attributes' => ['data-webform-composite-id' => $html_id . '--state_province'],
     ];
 
     return $elements;
