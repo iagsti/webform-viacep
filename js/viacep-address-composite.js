150f93027a453b2963c74a005fb1b84ee1126695
Marcelo Schneider <schneider.fei@gmail.com>
2018-10-31 13:15:02 -0300

    Allowing using viacep more than once - issue #12
    Allowing using viacep more than once one the same webform

--- js/viacep-address-composite.js
+++ js/viacep-address-composite.js
@@ -3,31 +3,36 @@
 
   Drupal.behaviors.viacepAddressComposite = {
     attach: function (context, settings) {
-      
-      let viacepFields = $('.viacep-address-composite--group', context).once('viacep-address-composite--group');
 
-      let postalCodeField = viacepFields.find('.viacep-address-composite--postal-code');
-      let addressField = viacepFields.find('.viacep-address-composite--address');
-      let neighborhood = viacepFields.find('.viacep-address-composite--neighborhood');
-      let cityField = viacepFields.find('.viacep-address-composite--city');
-      let stateField = viacepFields.find('.viacep-address-composite--state_province');
+      let viacepFields = $('.viacep-address-composite-js', context).once('viacep-address-composite-js');
 
       let viacep = new Viacep();
       let viacepRequest = null;
+      
+      let formName = '';
+      let postalCodeFields = viacepFields.find('.viacep-address-composite--postal-code');
+      let postalCodeField = null;
 
-      postalCodeField.focusout(function () {
+      postalCodeFields.focusout(function () {
 
-        viacep.setPostalCode(postalCodeField.val());
-        viacepRequest = viacep.createQuery();
+        postalCodeField = $(this);
 
-        viacep.getJSON(viacepRequest, function (response) {
+        if (postalCodeField.val()) {
 
-          addressField.val(response.logradouro);
-          neighborhood.val(response.bairro);
-          cityField.val(response.localidade);
-          stateField.val(response.uf);
-  
-        });
+          formName = postalCodeField.attr('name').split('[')[0];
+          viacep.setPostalCode(postalCodeField.val());
+          viacepRequest = viacep.createQuery();
+
+          viacep.getJSON(viacepRequest, function (response) {
+
+            viacepFields.find('[name="' + formName + '[address]"]').val(response.logradouro);
+            viacepFields.find('[name="' + formName + '[neighborhood]"]').val(response.bairro);
+            viacepFields.find('[name="' + formName + '[city]"]').val(response.localidade);
+            viacepFields.find('[name="' + formName + '[state_province]"]').val(response.uf);
+
+          });
+
+        }
 
       });
 
